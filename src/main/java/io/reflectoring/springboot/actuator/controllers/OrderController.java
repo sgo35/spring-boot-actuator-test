7c7d1a04e13db52bfbbcc0c10719074a31f1f48d
SGo35 <letsgototsm@outlook.fr>
2023-03-16 12:44:44 +0100

    commit reverse 2

--- src/main/java/io/reflectoring/springboot/actuator/controllers/OrderController.java
+++ src/main/java/io/reflectoring/springboot/actuator/controllers/OrderController.java
@@ -25,7 +25,7 @@ public class OrderController {
 
   @GetMapping("/{customerId}/orders")
   public List<Order> getOrders(@PathVariable(value = "customerId") String customerId) {
-    System.out.println("Get orders requested for customer SGO2 : " + customerId);
+    System.out.println("Get orders requested for customer SGO : " + customerId);
 
     // Dummy order history for example purpose
     OrderLineItem orderLineItem1 = new OrderLineItem();
